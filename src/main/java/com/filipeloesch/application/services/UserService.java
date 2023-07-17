bd52e62d13efc1a8b58c09756228b02ac2ffb7db
Filipe Loesch <filipe.loesch.ferreira@gmail.com>
2023-07-17 01:22:34 -0300

    Service Layer, Component Registration

---
+++ src/main/java/com/filipeloesch/application/services/UserService.java
@@ -0,0 +1,26 @@
+package com.filipeloesch.application.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.filipeloesch.application.entities.User;
+import com.filipeloesch.application.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
