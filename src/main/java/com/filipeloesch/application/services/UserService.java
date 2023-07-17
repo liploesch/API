051591898583e01b73356994c26cb856509b6a0b
Filipe Loesch <filipe.loesch.ferreira@gmail.com>
2023-07-17 18:38:02 -0300

    Exception Handling - findById

--- src/main/java/com/filipeloesch/application/services/UserService.java
+++ src/main/java/com/filipeloesch/application/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.filipeloesch.application.entities.User;
 import com.filipeloesch.application.repositories.UserRepository;
+import com.filipeloesch.application.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Integer id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
